9e1e140f0ccba8192c9f32a801aeda51c4e9e23d
macazure <38785252+macazure@users.noreply.github.com>
2020-11-24 16:45:01 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,20 +6,20 @@
 
 variable "client_id" {
     description =   "Client ID (APP ID) of the application"
-    type        =   string
+    type        =   "1a7578e2-18af-4f7e-a6a6-73e7da079ef4"
 }
 
 variable "client_secret" {
     description =   "Client Secret (Password) of the application"
-    type        =   string
+    type        =   "ySjyJAxH0z-NbyzX19-48ItwtVyVIgcXN"
 }
 
 variable "subscription_id" {
     description =   "Subscription ID"
-    type        =   string
+    type        =   "b2d070b2-b854-42f6-9307-ce32d71c4cbf"
 }
 
 variable "tenant_id" {
     description =   "Tenant ID"
-    type        =   string
+    type        =   "a306c256-8a1e-4588-8150-a3bb9751fdc8"
 }
