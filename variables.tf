d98b8958f1bee6bb3a5ab96d43455aabfbe01682
macazure <38785252+macazure@users.noreply.github.com>
2020-11-30 17:21:43 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,20 +6,20 @@
 
 variable "client_id" {
     description =   "Client ID (APP ID) of the application"
-    type        =   "string"
+    type        =   string
 }
 
 variable "client_secret" {
     description =   "Client Secret (Password) of the application"
-    type        =   "string"
+    type        =   string
 }
 
 variable "subscription_id" {
     description =   "Subscription ID"
-    type        =   "string"
+    type        =   string
 }
 
 variable "tenant_id" {
     description =   "Tenant ID"
-    type        =   "string"
+    type        =   string
 }
