c0560b0b1d1c39d85c6019ab8afc8354de2f35e1
macazure <38785252+macazure@users.noreply.github.com>
2020-11-30 17:24:38 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@
 
 variable "client_id" {
     description =   "Client ID (APP ID) of the application"
-    type        =   69a8c671-f14e-4195-b3ed-9f9cc13e7b09
+    type        =   "9a8c671-f14e-4195-b3ed-9f9cc13e7b09"
 }
 
 variable "client_secret" {
