1299e65dc12b85f3ad72b22dbe5f1125114a464a
macazure <38785252+macazure@users.noreply.github.com>
2020-12-01 18:24:02 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
         resource_group_name     =   "rg-terraformstatemac"
         storage_account_name    =   "terrastatestorage180"
         container_name          =   "terraformdemomac"
-        key                     =   "key1"
+        key                     =   "access_key"
     }
 }
 
